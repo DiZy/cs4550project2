449393bbf91b24c3166640a3b43009fe66be0957
davidzilburg <zilburg.d@husky.neu.edu>
2019-04-04 18:19:35 -0400

    more db seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,9 +12,16 @@
 alias Project2.Repo
 alias Project2.Users.User
 alias Project2.MemesFound.MemeFound
+alias Project2.UserCreatedMemes.UserCreatedMeme
+alias Project2.ActiveMemes.ActiveMeme
 
 pwhash = Argon2.hash_pwd_salt("pass1")
 
 Repo.insert!(%User{email: "alice@example.com", admin: true, password_hash: pwhash})
 Repo.insert!(%User{email: "bob@example.com", admin: false, password_hash: pwhash})
+Repo.insert!(%UserCreatedMeme{image_url: "idk", text_line_one: "this is meme", text_line_two: "yay"})
+Repo.insert!(%UserCreatedMeme{image_url: "idk2", text_line_one: "this is meme2", text_line_two: "yay2"})
 Repo.insert!(%MemeFound{user_id: 1, gif_id: "YsTs5ltWtEhnq", is_user_created: false})
+Repo.insert!(%MemeFound{user_id: 1, meme_id: 1, is_user_created: true})
+Repo.insert!(%ActiveMeme{lat: 42.339411999999996, long: -71.0830491, gif_id: "l3JDFJncJHteKIYzm", is_user_created: false})
+Repo.insert!(%ActiveMeme{lat: 43.339411999999996, long: -71.0830491, meme_id: 1, is_user_created: true})
